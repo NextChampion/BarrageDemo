bd0d33bf2c71bd4b227b0cdbc84dc167ac3b875a
zhangcunxia <zcx4150@163.com>
2019-01-28 18:38:52 +0800

    修改缩进

--- BarrageExample/src/components/BarrageInputView.js
+++ BarrageExample/src/components/BarrageInputView.js
@@ -1,78 +1,78 @@
 
 import React, { Component } from 'react';
 import {
-  StyleSheet,
-  Text,
-  View,
-  TextInput,
-  TouchableOpacity,
-  Alert
+    StyleSheet,
+    Text,
+    View,
+    TextInput,
+    TouchableOpacity,
+    Alert
 } from 'react-native';
 import PropTypes from 'prop-types';
 import UI from '../UI';
 
 export default class BarrageInputView extends Component {
-  constructor(props) {
-    super(props);
-    this.textInput = '';
-  }
+    constructor(props) {
+        super(props);
+        this.textInput = '';
+    }
 
-  static propTypes = {
-    onButtonPress: PropTypes.func,
-  };
+    static propTypes = {
+        onButtonPress: PropTypes.func,
+    };
 
-  static defaultProps = {
-    onButtonPress: null,
-  };
+    static defaultProps = {
+        onButtonPress: null,
+    };
 
-  render() {
-    const { onButtonPress } = this.props;
-    return (
-      <View style={styles.textInputContainer}>
-        <TextInput placeholder={'请输入弹幕内容'} style={styles.textInput} onChangeText={(text) => {
-          this.textInput = text.trim();
-        }} />
-        <TouchableOpacity
-          onPress={() => {
-            if (!this.textInput) {
-              Alert.alert('请输入内容');
-              return;
-            }
-            if (onButtonPress) {
-              onButtonPress(this.textInput);
-            }
-          }}
-          style={styles.button}
-        >
-          <Text>发送</Text>
-        </TouchableOpacity>
-      </View>
-    )
-  }
+    render() {
+        const { onButtonPress } = this.props;
+        return (
+            <View style={styles.textInputContainer}>
+                <TextInput placeholder={'请输入弹幕内容'} style={styles.textInput} onChangeText={(text) => {
+                    this.textInput = text.trim();
+                }} />
+                <TouchableOpacity
+                    onPress={() => {
+                        if (!this.textInput) {
+                            Alert.alert('请输入内容');
+                            return;
+                        }
+                        if (onButtonPress) {
+                            onButtonPress(this.textInput);
+                        }
+                    }}
+                    style={styles.button}
+                >
+                    <Text>发送</Text>
+                </TouchableOpacity>
+            </View>
+        )
+    }
 
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    paddingTop: 34,
-    backgroundColor: '#F5FCFF',
-  },
-  barrageView: {
-    height: UI.size.screenHeight / 9,
-  },
-  textInputContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    backgroundColor: 'red',
-    paddingLeft: 24,
-  },
-  textInput: {
-    backgroundColor: 'gray',
-    height: 40,
-    width: 200,
-  },
-  button: {
-    paddingHorizontal: 12,
-  }
+    container: {
+        flex: 1,
+        paddingTop: 34,
+        backgroundColor: '#F5FCFF',
+    },
+    barrageView: {
+        height: UI.size.screenHeight / 9,
+    },
+    textInputContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        backgroundColor: 'red',
+        paddingLeft: 24,
+    },
+    textInput: {
+        backgroundColor: 'gray',
+        height: 40,
+        width: 200,
+    },
+    button: {
+        paddingHorizontal: 12,
+    }
 });
